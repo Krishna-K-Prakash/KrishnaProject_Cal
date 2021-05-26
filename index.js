6cba21e83e467f304aea15cb7507abc5eed01352
Krishna <krishnakprakash123@gmail.com>
2021-05-26 11:44:36 +0530

    Whole year calendar done

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const date = new Date();
-const monthDays = document.querySelector('days');
+
+const Calendar = () => {
+    date.setDate(1);
+const lastDate = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+const prevDate = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+const firstDayIndex = date.getDay();
+const lastDateIndex = new Date(date.getFullYear(), date.getMonth() + 1,0).getDay();
+console.log(lastDateIndex);
+const nextDays = 7 - lastDateIndex - 1;
 const months = ["January",
 "February",
 "March",
@@ -16,10 +24,38 @@ const months = ["January",
 document.querySelector(".date h1").innerHTML = months[date.getMonth()];
 document.querySelector(".date p").innerHTML = date.toDateString();
 
-let days = "";
-for(let i = 1; i<= 31;i++){
-    days += `<div>${i}</div>`;
-    monthDays.innerHTML = days;
-};
+let Days = ' ';
+
+for(let x = firstDayIndex; x>0; x--){
+    Days += `<div class='prev-date'>${prevDate - (x-1)
+    
+    }</div>`;
+}
+
+for(let i = 1; i<= 31 ;i++){
+    if(i == new Date().getDate()
+    && date.getMonth()== new Date().getMonth()){
+        Days += `<div class="today">${i}</div>`; 
+    }
+    else{
+    Days += `<div>${i}</div>`;
+    }
+}
+
+for(let j=1; j <= nextDays; j++){
+    Days += `<div class="next-date">${j}</div>`;
+    document.querySelector(".days").innerHTML = Days;
+}
+
+}
+
+document.querySelector('.prev').addEventListener('click',() => {date.setMonth(date.getMonth() - 1);
+    Calendar();
+});
+
+document.querySelector('.next').addEventListener('click',() => {date.setMonth(date.getMonth() + 1);
+    Calendar();
+});
 
 
+Calendar()
