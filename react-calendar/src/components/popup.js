a89e2207db5e7f6ff3de14f753d6a5a902091c75
Krishna <krishnakprakash123@gmail.com>
2021-06-04 17:36:09 +0530

    added more features to popup

--- react-calendar/src/components/popup.js
+++ react-calendar/src/components/popup.js
@@ -1,19 +1,62 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../popup.css';
 
+
 function Popup(props) {
+
+    const [getPopups, setGetPopups] = useState([])
+    const[popups, setPopups] = useState("")
+
+    useEffect(() => {
+        const temp = localStorage.getItem("data")
+        const loadedItem = JSON.parse(temp)
+
+        if(loadedItem) {
+            setGetPopups(loadedItem)
+        }
+    }, [] )
+
+    useEffect(() => {
+        const temp = JSON.stringify(getPopups)
+        localStorage.setItem("data", temp)
+    }, [getPopups])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const newPopup = {
+            id : new Date().getTime(),
+            text: popups,
+            completed:false,
+        }
+
+        setGetPopups([...getPopups].concat(newPopup))
+        setPopups("")
+    }
+
+    const deleteEvent = (id) => {
+        const updatedPopup = [...getPopups].filter((popups) => popups.id !== id)
+        setGetPopups(updatedPopup)
+    }
+ 
     return (props.trigger) ? 
      (  
         <div className="popupForm">
             <div className="header">
                 <lable className="event">+ ADD EVENT</lable>
             </div>
-            <div>
-                <input type = "text" placeholder="Add Event" id="myInput" />
-                <br />
+            
+            <form onSubmit = {handleSubmit}>
+                <input type="text" placeholder="Add Event" onChange={(e) => setPopups(e.target.value)} value={popups} />
                 <button className="popup-cancel" onClick = {() => props.setTrigger(false)}>Cancel</button>
-                <button className="popup-confirm">Add</button>
-                { props.children }
+                <button type="submit" className="popup-confirm">Add</button>
+            </form>
+            <div className="show-data">
+            {getPopups.map((popups) => 
+                <div className="eventData"> 
+                    <p>{popups.text}</p>
+                    <button><i className="fas fa-times-circle" onClick = {() => deleteEvent(popups.id)}></i></button>
+                </div>)}
             </div>
         </div> 
      ) : "";
